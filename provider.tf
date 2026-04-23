c534c0ee7fd4454a45d5ebc67c5896aa1875cb33
Avinash0397 <avimishra478@gmail.com>
2026-04-23 12:53:43 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,19 +6,22 @@ terraform {
   }
 }
 terraform {
-  required_version = ">= 0.15.0"
+  required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.29.0"
     }
+
     random = {
       source  = "hashicorp/random"
-      version = ">= 4.0.0"
+      version = ">= 3.5.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = ">= 4.0.0"
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.10"
     }
   }
 }
