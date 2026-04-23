9f9d999934c39a6ae2fff57f2c6db936f558d7e1
Avinash0397 <avimishra478@gmail.com>
2026-04-23 12:56:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -18,11 +18,13 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.5.0"
     }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.10"
-    }
+  }
+}
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
   }
 }
 provider "aws" {
